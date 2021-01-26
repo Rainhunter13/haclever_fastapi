fff345678be9093706cfbf591cd47dbb9ee8cf7a
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-01-27 00:55:34 +0600

    Added pydentic models for Tariff and Cargo

--- models.py
+++ models.py
@@ -1,14 +1,21 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class Tariff(Model):
+    id = fields.IntField(pk=True)
     date = fields.DateField(default=None)
     cargo_type = fields.CharField(max_length=100, default=None)
     rate = fields.FloatField(default=None)
 
 
 class Cargo(Model):
+    id = fields.IntField(pk=True)
     cargo_type = fields.CharField(max_length=100, default=None)
     declared_cost = fields.FloatField(default=None)
     insurance_cost = fields.FloatField(default=None)
+
+
+TariffPydantic = pydantic_model_creator(Tariff, name="TariffPydantic")
+CargoPydantic = pydantic_model_creator(Tariff, name="CargoPydantic")
