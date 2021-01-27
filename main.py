2f899003130c66f0ff611c14e6aba301b8a3c418
Rakhman Asmatullayev <rakhmandva@gmail.com>
2021-01-27 17:15:08 +0600

    Added get cargos endpoint

--- main.py
+++ main.py
@@ -36,3 +36,8 @@ async def insurance_cost(cargo: CargoPydantic):
     await calculate_insurance_cost(cargo)
     await Cargo.create(**cargo.dict())
     return cargo
+
+
+@app.get("/cargos", response_model=List[CargoPydantic])
+async def get_cargos():
+    return await CargoPydantic.from_queryset(Cargo.all())
